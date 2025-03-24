73f039f9b1efc4c24efc6d5d73814a920e41c859
Kirill Udaltsov <commits@udaltsovk.ru>
2025-03-24 04:00:25 +0300

    fix(backend): ignore /metrics requests

--- backend/crates/api/src/utils/logger.rs
+++ backend/crates/api/src/utils/logger.rs
@@ -6,14 +6,14 @@ pub struct CustomLogger;
 impl CustomLogger {
     #[allow(clippy::new_ret_no_self)]
     pub fn new() -> Logger {
-        Logger::new("%a \"%r\" %s (took %D ms to serve)").custom_level(
-            |status| {
+        Logger::new("%a \"%r\" %s (took %D ms to serve)")
+            .exclude("/metrics")
+            .custom_level(|status| {
                 if status.is_server_error() {
                     Level::Error
                 } else {
                     Level::Debug
                 }
-            },
-        )
+            })
     }
 }
