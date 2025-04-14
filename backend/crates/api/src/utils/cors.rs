57f972762e352f4f9644a30c6d340abcd4d01634
Kirill Udaltsov <commits@udaltsovk.ru>
2025-04-14 03:16:08 +0300

    feat(backend/api): configure cors

--- backend/crates/api/src/utils/cors.rs
+++ backend/crates/api/src/utils/cors.rs
@@ -1,6 +1,25 @@
 use actix_cors::Cors;
+use actix_web::http::header;
+
+use crate::config;
 
 #[tracing::instrument(level = "trace")]
 pub fn default_cors() -> Cors {
-    Cors::permissive()
+    if config::DEPLOY_DOMAIN.as_str() == "localhost" {
+        Cors::permissive()
+    } else {
+        Cors::default()
+            .allowed_origin(&format!(
+                "https://{}",
+                config::DEPLOY_DOMAIN.clone()
+            ))
+            .allowed_methods(vec!["GET", "POST", "PUT", "PATCH", "DELETE"])
+            .allowed_headers(vec![
+                header::AUTHORIZATION,
+                header::ACCEPT,
+                header::ACCEPT_ENCODING,
+            ])
+            .supports_credentials()
+            .block_on_origin_mismatch(true)
+    }
 }
