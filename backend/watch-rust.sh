814d24c13b15b6b1cf752d26d4b3901dba63bc09
Kirill Udaltsov <commits@udaltsovk.ru>
2025-04-20 15:29:15 +0300

    feat(backend): make watch-rust.sh usable even offline

--- backend/watch-rust.sh
+++ backend/watch-rust.sh
@@ -3,7 +3,7 @@ RUSTFLAGS="-Z macro-backtrace" watchexec \
     -rqc reset \
     -e rs,toml "
     cargo udeps --all && \
-    cargo audit && \
+    cargo audit -n && \
     cargo fmt --all && \
     cargo clippy --all -- -D warnings && \
     cargo test --all && \
