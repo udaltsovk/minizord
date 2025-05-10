16a5c7088e1358c4b19ba083e83d3ca150c50870
Kirill Udaltsov <commits@udaltsovk.ru>
2025-05-10 22:30:48 +0300

    feat(backend): make logger more configurable

--- backend/crates/utils/src/logger.rs
+++ backend/crates/utils/src/logger.rs
@@ -1,42 +1,21 @@
 #[cfg(feature = "actix-web")]
-use {actix_contrib_logger::middleware::Logger, tracing::log::Level};
+use {
+    actix_contrib_logger::middleware::Logger, actix_web::http::StatusCode,
+    tracing::log::Level,
+};
 
 #[cfg(feature = "actix-web")]
 pub struct CustomActixLogger;
 #[cfg(feature = "actix-web")]
 impl CustomActixLogger {
-    #[allow(
-        clippy::new_ret_no_self,
-        clippy::match_single_binding,
-        clippy::if_same_then_else
-    )]
+    #[allow(clippy::new_ret_no_self)]
     pub fn new() -> Logger {
-        Logger::new("%{r}a \"%r\" %s (took %D ms to serve)")
+        Logger::new("%a \"%r\" %s (took %D ms to serve)")
             .exclude("/metrics")
-            .custom_level(|status| {
-                if status.is_server_error() {
-                    match status {
-                        _ => Level::Error,
-                    }
-                } else if status.is_client_error() {
-                    match status {
-                        _ => Level::Info,
-                    }
-                } else if status.is_redirection() {
-                    match status {
-                        _ => Level::Info,
-                    }
-                } else if status.is_success() {
-                    match status {
-                        _ => Level::Info,
-                    }
-                } else if status.is_informational() {
-                    match status {
-                        _ => Level::Info,
-                    }
-                } else {
-                    Level::Info
-                }
+            .custom_level(|status| match status {
+                StatusCode::METHOD_NOT_ALLOWED => Level::Warn,
+                _ if status.is_server_error() => Level::Error,
+                _ => Level::Info,
             })
     }
 }
